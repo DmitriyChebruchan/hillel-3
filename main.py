22cee81f1b61055d2b3b91bbe6848862b9424087
DmitriyChebruchan <chebruchan2707@gmail.com>
2023-09-15 22:12:20 +0300

    function for calculating average was added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from faker import Faker
+import csv
 
 fake = Faker()
 
@@ -44,3 +45,35 @@ def user_generator():
     for i in range(total_quantity):
         result += str(i + 1) + ". " + fake.name() + " " + fake.email() + "<br>"
     return result
+
+
+@app.route("/mean/")
+def mean():
+    try:
+        with open("hw.csv", mode="r") as file:
+            csv_reader = csv.reader(file)
+            total_rows = 0
+            sum_column1 = 0
+            sum_column2 = 0
+
+            for row in csv_reader:
+                if len(row) >= 2:
+                    try:
+                        column1 = float(row[1])
+                        column2 = float(row[2])
+                        sum_column1 += column1
+                        sum_column2 += column2
+                        total_rows += 1
+                    except ValueError:
+                        continue  # Skip rows with non-numeric data in columns
+
+            if total_rows == 0:
+                return "No valid data found in the CSV file."
+
+            average_column1 = round(sum_column1 / total_rows * 2.54, 2)
+            average_column2 = round(sum_column2 / total_rows * 0.453592, 2)
+
+            return f"Average of height: {average_column1} cm, Average of weight: {average_column2} kg"
+
+    except FileNotFoundError:
+        return "File not found."
